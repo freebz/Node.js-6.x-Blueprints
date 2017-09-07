b36aaec178db9242ba9c95a07b60962efb502df7
FreeBz <freebz@hananet.net>
2017-09-07 11:03:11 +0900

    1장 - MVC 디자인 패턴을 이용한 트위터스타일 애플리케이션 만들기

--- ch01/app.js
+++ ch01/app.js
@@ -23,7 +23,7 @@ var comments = require('./server/controllers/comments');
 
 var app = express();
 
-// view engine setup
+// 뷰 엔진 설정
 app.set('views', path.join(__dirname, 'server/views/pages'));
 app.set('view engine', 'ejs');
 
@@ -81,7 +81,7 @@ app.use('/users', users);
 app.get('/comments', comments.hasAuthorization, comments.list);
 app.post('/comments', comments.hasAuthorization, comments.create);
 
-// catch 404 and forward to error handler
+// 404 에러가 잡히면 에러 핸들러에 전송
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
